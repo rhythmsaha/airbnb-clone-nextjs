49ab97eed94cddfd363f39d0bbb2efaf60758773
Rhythm Saha <rs.2001.saha@gmail.com>
2021-08-05 10:58:47 +0530

    test

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,9 @@ import {
 
 function Header() {
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-2 px-5 md:px-10">
             {/* left */}
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div className="relative flex items-center h-7 cursor-pointer my-auto">
                 <Image
                     src="https://links.papareact.com/qd3"
                     layout="fill"
---
+++ pages/test.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+function Test() {
+    return (
+        <div>
+            <h1>Test</h1>
+            <Link href="/">Home</Link>
+        </div>
+    )
+}
+
+export default Test
